00d060c087f72cbd74793fe8ecd2156ea0f18c1f
Andrea <andrea.antillon.b@gmail.com>
2020-02-29 11:54:15 -0700

    Maximum number input implemented.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,35 @@ namespace GuessTheNumber
 {
     class Program
     {
+        static int RangeSize = 0;
+
+        static void InputRangeSize()
+        {
+            bool isRangeDefined = false;
+
+            while (!isRangeDefined) 
+            {
+                Console.WriteLine("Hello, welcome to Guess The Number game!");
+                Console.WriteLine("Please input a positive number as the maximum.");
+                string userNumberInput = Console.ReadLine();
+
+                RangeSize = Convert.ToInt32(userNumberInput);
+
+                //Check if input is positive and is greater than 0
+                if (RangeSize > 0)
+                {
+                    isRangeDefined = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input! Please try again");
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            InputRangeSize();
+            Console.WriteLine("Maximum Number is: " + RangeSize);
         }
     }
 }
